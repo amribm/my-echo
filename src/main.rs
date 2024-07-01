5437552c14fbc4843a0ac4bc0443ac8c0573f76d
i-ai-dev <ameerthecoder@gmail.com>
2024-07-01 17:57:01 +0530

    refactor(core): argument parsing using clap and writing to std using io::stdout()
    - parsing args using clap
    - -n for no-newline at the trailing end
    - writing to stdout using io::stdout()

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-use std::env;
+use clap::{Arg, ArgAction, Command};
+use std::{
+    env,
+    io::{self, Write},
+};
 
-fn main() {
-    let args = args_os();
-    write_args(args)
+mod options {
+    pub const NO_NEWLINE: &str = "no_newline";
+    pub const STRING: &str = "STRING";
 }
 
-fn args_os() -> Vec<String> {
-    env::args_os()
-        .into_iter()
-        .map(|x| x.to_string_lossy().into_owned().as_str().to_owned())
-        .collect()
+fn main() -> io::Result<()> {
+    let matches = app_commmad().get_matches_from(env::args_os());
+    let no_newline = matches.get_flag(options::NO_NEWLINE);
+    let all = match matches.get_many::<String>(options::STRING) {
+        Some(s) => s.map(|s| s.to_string()).collect(),
+        None => vec![String::new()],
+    };
+    write_args(no_newline, all)
 }
 
-fn write_args(args: Vec<String>) {
-    if args.len() > 1 {
-        println!("{}", args[1..].join(" "));
+fn write_args(no_newline: bool, args: Vec<String>) -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    if !no_newline {
+        writeln!(stdout, "{}", args.join(" "))?;
     } else {
-        println!("");
+        write!(stdout, "{}", args.join(" "))?;
     }
+
+    Ok(())
+}
+
+fn app_commmad() -> Command {
+    Command::new("my-echo")
+        .bin_name("/target/debug/my-echo")
+        .dont_delimit_trailing_values(true)
+        .about("my-echo - write arguments to the standard output")
+        .arg(
+            Arg::new(options::NO_NEWLINE)
+                .short('n')
+                .help("do not output trailing `\n`")
+                .action(ArgAction::SetTrue)
+                .overrides_with(options::NO_NEWLINE),
+        )
+        .arg(Arg::new(options::STRING).action(ArgAction::Append))
 }
