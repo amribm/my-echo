fa3298b0ba1a8f712cfd71fcae84d47eb4e5ebb0
i-ai-dev <ameerthecoder@gmail.com>
2024-07-01 11:00:21 +0530

    refactor(core): change for replacing OsString with String

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, ffi::OsString};
+use std::env;
 
 fn main() {
-    let args: Vec<_> = env::args_os().collect();
+    let args: Vec<_> = env::args_os()
+        .into_iter()
+        .map(|x| x.to_string_lossy().into_owned().as_str().to_owned())
+        .collect();
 
     if args.len() > 1 {
-        println!("{:?}", args[1..].join(&OsString::from(" ")));
+        println!("{}", args[1..].join(" "));
     } else {
         println!("");
     }
