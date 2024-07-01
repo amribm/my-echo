73d98a1e6b86fa40716664b25dd7eb295b5accb0
i-ai-dev <ameerthecoder@gmail.com>
2024-07-01 13:09:46 +0530

    refactor(core): seperating the parts into invidiual function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
 
 fn main() {
-    let args: Vec<_> = env::args_os()
+    let args = args_os();
+    write_args(args)
+}
+
+fn args_os() -> Vec<String> {
+    env::args_os()
         .into_iter()
         .map(|x| x.to_string_lossy().into_owned().as_str().to_owned())
-        .collect();
+        .collect()
+}
 
+fn write_args(args: Vec<String>) {
     if args.len() > 1 {
         println!("{}", args[1..].join(" "));
     } else {
